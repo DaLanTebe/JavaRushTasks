0cb69353fa02755d9554c555ac2346522df389bc
daniil <danilka.radaev.00@mail.ru>
2022-08-04 19:11:00 +0300

    add solution javaSyntax from task 0103

--- 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -6,8 +6,8 @@ package com.javarush.task.task01.task0105;
 
 public class Solution {
     public static void main(String[] args) {
-        String main;
-        int
-
+        String name;
+        int age;
+        String city;
     }
 }
