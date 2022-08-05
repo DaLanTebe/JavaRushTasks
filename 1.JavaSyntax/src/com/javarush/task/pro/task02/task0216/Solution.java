742f05c17e505ee168ab91320f7b5c85ee0b7a52
daniil <danilka.radaev.00@mail.ru>
2022-08-05 19:43:40 +0300

    add solution javaSyntax from task 0216

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "dan";
+        String date = "312";
+        String time = "req";
+        String noun = "fds";
+        String adjective = "qrw";
+        String verb = "4214";
+
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}
