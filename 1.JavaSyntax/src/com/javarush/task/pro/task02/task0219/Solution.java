14f902e28da51696a6b4fa8729a4b6bb1baa38ea
daniil <danilka.radaev.00@mail.ru>
2022-08-05 19:22:20 +0300

    add solution javaSyntax from task 0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       int id = 12023;
+       int age = 120;
+       String documents = "law";
+       String behavior = "good";
+    }
+}
