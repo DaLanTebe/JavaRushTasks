3d7b32c22c8eb03ffc7d17023a92e74697b75b67
daniil <danilka.radaev.00@mail.ru>
2022-08-05 19:09:00 +0300

    add solution javaSyntax from task 0109

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0202/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0202;
+
+/* 
+Полнометражная картина
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int centimeters = 243;
+        int x = centimeters / 100;
+        int meters = x;
+
+
+        System.out.println(meters);
+    }
+}
