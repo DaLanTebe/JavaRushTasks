99c0645d92c0c84eb7f8e3a1c4985cdd94ccd30f
daniil <danilka.radaev.00@mail.ru>
2022-08-07 20:56:21 +0300

    add solution javaSyntax from task 0318

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0318/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task03.task0318;
+
+import java.util.Scanner;
+
+/* 
+Био-мясо
+*/
+
+public class Solution {
+    public static String NOT_YET = "Искусственное мясо еще не готово.";
+    public static String DONE = "Искусственное мясо готово.";
+    public static String HORRIBLY_DONE = "Искусственное мясо превратилось в мясного монстра!";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+        int minutes = scanner.nextInt();
+        int time = (hours * 60) + minutes;
+        if (time == 30) {
+            System.out.println(DONE);
+        }else if (time < 30) {
+            System.out.println(NOT_YET);
+        }else if (time > 30)
+            System.out.println(HORRIBLY_DONE);
+    }
+}
