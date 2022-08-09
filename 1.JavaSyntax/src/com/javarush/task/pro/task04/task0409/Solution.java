fded2d9084814f360fe903cab20bdc34062c2027
daniil <danilka.radaev.00@mail.ru>
2022-08-09 23:20:35 +0300

    add solution javaSyntax from task 0409

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0409/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task04.task0409;
+
+import java.util.Scanner;
+
+/* 
+Минимум из введенных чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int min = Integer.MAX_VALUE;
+        int a;
+        while (scanner.hasNextInt()) {
+            a = scanner.nextInt();
+            if (min > a) {
+                min = a;
+            }
+        }
+        System.out.println(min);
+    }
+}
