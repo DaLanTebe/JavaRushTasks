9ef34451ef21b2e29155c57ce3703d1b4f077a6c
daniil <danilka.radaev.00@mail.ru>
2022-08-11 19:02:42 +0300

    add solution javaSyntax from task 0425

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -15,7 +15,14 @@ public class Solution {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             for (int k = 0; k < 10; k++) {
-                System.out.print(land);
+                if (i == 0 && k == 3){
+                    System.out.print(robot);
+                }
+                else if (i == 9){
+                    System.out.print(bug);
+                }else {
+                    System.out.print(land);
+                }
             }
         System.out.println("");
         }
