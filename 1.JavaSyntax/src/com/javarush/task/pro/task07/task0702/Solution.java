c25ead998e9b86ccace308253fc57c9eecd1718a
daniil <danilka.radaev.00@mail.ru>
2022-08-23 19:05:54 +0300

    add solution javaSyntax from task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    int a = Integer.MAX_VALUE;
+    byte b = Byte.MAX_VALUE;
+    short c = Short.MAX_VALUE;
+    long d = Long.MAX_VALUE;
+}
