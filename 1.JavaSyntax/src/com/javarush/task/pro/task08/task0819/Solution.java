2c5e93485fb53e02fe7ce42a6c1640ad25d09cb2
daniil <danilka.radaev.00@mail.ru>
2022-09-04 20:21:53 +0300

    add solution javaSyntax from task 0819

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0819/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0819/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.pro.task08.task0819;
 
+import java.lang.reflect.Field;
 import java.util.Random;
 
 /* 
@@ -10,10 +11,24 @@ import static com.javarush.task.pro.task08.task0819.CosmicAnomaly.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Random random = new Random();
-        for (int i = 0; i < 30; i++) {
-            CosmicAnomaly.nextAttempt(random.nextInt(upperBound));
-        }
+        int number = 0;
+        int result = 0;
+        do {
+            number = random.nextInt(upperBound - lowerBound) + lowerBound;
+            result = nextAttempt(number);
+        } while (number != result);
+
+//           Field declaredField = CosmicAnomaly.class.getDeclaredField("rightNumber");
+//
+//            declaredField.setAccessible(true);
+//            int number2 = (int) declaredField.get(new CosmicAnomaly());
+//
+//            if (number == number2) {
+//                break;
+//            }
     }
 }
+
+
