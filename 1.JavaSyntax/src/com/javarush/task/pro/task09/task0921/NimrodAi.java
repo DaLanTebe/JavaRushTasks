1b04a4eb1dd9d4a40f2017e88f91ae7a5b129604
daniil <danilka.radaev.00@mail.ru>
2022-09-07 20:02:43 +0300

    add solution javaSyntax from task 0921

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0921/NimrodAi.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0921/NimrodAi.java
@@ -11,7 +11,7 @@ public class NimrodAi {
     public static void main(String[] args) {
         PastryLoader[] pastryLoaders = new PastryLoader[]{new PastryLoader(), new PastryLoader(), new PastryLoader()};
 
-        scanMemory(pastryLoaders);
+       scanMemory(pastryLoaders);
         replaceTarget(pastryLoaders);
         scanMemory(pastryLoaders);
     }
@@ -24,6 +24,19 @@ public class NimrodAi {
     }
 
     public static void replaceTarget(PastryLoader[] pastryLoaders) {
-
+        for (PastryLoader pastryLoader : pastryLoaders) {
+            String[][][] array = pastryLoader.memory;
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    for (int k = 0; k < array[i][j].length; k++) {
+                        String string = array[i][j][k];
+                        String str = string.toLowerCase();
+                        if (str.contains("nimrod")) {
+                            array[i][j][k] = str.replaceAll("nimrod", "pirate ship");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
