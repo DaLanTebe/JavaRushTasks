60c10c4e9996001de8e177281005d14ac4cc0c0a
daniil <danilka.radaev.00@mail.ru>
2022-10-19 18:45:48 +0300

    add solution javaSyntax from task 1523

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1523/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1523/Solution.java
@@ -11,7 +11,20 @@ import java.net.URLConnection;
 public class Solution {
     public static void main(String[] args) throws IOException {
         URL url = new URL("http://httpbin.org/post");
-        //напишите тут ваш код
+        URLConnection connection = url.openConnection();
+
+        connection.setDoOutput(true);
+        OutputStream outputStream = connection.getOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        printStream.println("");
+
+        InputStream inputStream = connection.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        while (reader.ready()){
+            System.out.println(reader.readLine());
+        }
+
     }
 }
 
