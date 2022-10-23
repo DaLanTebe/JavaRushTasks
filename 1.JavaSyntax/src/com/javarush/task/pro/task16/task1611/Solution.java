73575fd8410b933d1dd1858759d1c056f91f02d1
daniil <danilka.radaev.00@mail.ru>
2022-10-23 21:56:01 +0300

    add solution javaSyntax from task 1611

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1611/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task16.task1611;
+
+import java.time.LocalTime;
+
+/* 
+Нужно просто посчитать
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws InterruptedException {
+        LocalTime localTime = LocalTime.MIDNIGHT;
+        LocalTime next = amazingMethod(localTime);
+        while (next.isAfter(localTime)) {
+            System.out.println(next);
+            next = amazingMethod(next);
+            Thread.sleep(500);
+        }
+    }
+
+    static LocalTime amazingMethod(LocalTime base) {
+        if (base == LocalTime.of(12, 0)){
+            return base.minusHours(12);
+        }
+            return base.plusHours(3);
+    }
+}
