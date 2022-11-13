312f908dde37df19648febea86268a06e23a30db
daniil <danilka.radaev.00@mail.ru>
2022-11-13 20:26:50 +0300

    add solution javaCore from task 1529

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements CanFly{
+
+    public int value;
+
+    public Plane(int count){
+        value = count;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
