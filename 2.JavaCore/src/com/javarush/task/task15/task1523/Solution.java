0a8100befaae30455f6cc2d9a48265fcddef704e
daniil <danilka.radaev.00@mail.ru>
2022-11-14 21:55:03 +0300

    add solution javaCore from task 1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    private Solution() {
+    }
+    public Solution(int a) {
+    }
+     Solution(String a) {
+    }
+    protected Solution(Character a) {
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+    public SubSolution(int a) {
+        super(a);
+    }
+
+    protected SubSolution(String a) {
+        super(a);
+    }
+
+     SubSolution(Character a) {
+        super(a);
+    }
+}
