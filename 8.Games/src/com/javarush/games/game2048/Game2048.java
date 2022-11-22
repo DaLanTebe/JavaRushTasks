bd2ecda724e42f41a4fc0006174b847bd985681b
daniil <danilka.radaev.00@mail.ru>
2022-11-22 21:06:24 +0300

    add solution javaGames from game2048.4/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -1,5 +1,6 @@
 package com.javarush.games.game2048;
 
+import com.javarush.engine.cell.Color;
 import com.javarush.engine.cell.Game;
 
 public class Game2048 extends Game {
@@ -11,12 +12,18 @@ public class Game2048 extends Game {
     public void initialize() {
         setScreenSize(SIDE, SIDE);
         createGame();
+        drawScene();
     }
 
-    private void createGame(){
+    private void createGame() {
     }
 
-    public static void main(String[] args) {
-
+    private void drawScene() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                setCellColor(i, j, Color.RED);
+            }
+        }
     }
+
 }
