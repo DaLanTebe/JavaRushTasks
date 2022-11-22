0442178747eafec0d414a4683bc1dd0d7297cf67
daniil <danilka.radaev.00@mail.ru>
2022-11-22 20:26:01 +0300

    add solution javaGames from game2048.2/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -1,7 +1,17 @@
 package com.javarush.games.game2048;
 
-import com.javarush.engine.cell.*;
+import com.javarush.engine.cell.Game;
 
 public class Game2048 extends Game {
 
+    private static final int SIDE = 4;
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
