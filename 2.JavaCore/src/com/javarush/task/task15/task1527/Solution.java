c73b0f46fb5048f48f666ed3967b6ba74408325c
daniil <danilka.radaev.00@mail.ru>
2022-11-22 20:26:58 +0300

    add solution javaCore from task 1827

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task15.task1527;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -12,7 +13,27 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String url = reader.readLine();
-        //напишите тут ваш код
+        String[] words = url.substring(url.lastIndexOf("?") + 1).split("&");
+
+        for (String word : words) {
+            if(word.contains("=")){
+                System.out.print(word.substring(0, word.indexOf("=")) + " ");
+            }else {
+                System.out.print(word + " ");
+            }
+        }
+        System.out.println("");
+        for (String word : words) {
+            if (word.contains("obj")){
+                String substring = word.substring(word.indexOf("=") + 1);
+                try {
+                    alert(Double.parseDouble(substring));
+                }catch (NumberFormatException exception){
+                    alert(substring);
+                }
+            }
+        }
+
     }
 
     public static void alert(double value) {
