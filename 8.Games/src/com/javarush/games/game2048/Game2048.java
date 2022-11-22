653b8cb2a2657db6d708a43aad861cb74440db3a
daniil <danilka.radaev.00@mail.ru>
2022-11-22 23:07:35 +0300

    add solution javaGames from game2048.6/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -20,26 +20,86 @@ public class Game2048 extends Game {
         createNewNumber();
     }
 
-    private void createNewNumber(){
+    private void createNewNumber() {
 
-            boolean isCreated = false;
-            do {
-                int x = getRandomNumber(SIDE);
-                int y = getRandomNumber(SIDE);
-                if (gameField[y][x] == 0) {
-                    gameField[y][x] = getRandomNumber(10) < 9 ? 2 : 4;
-                    isCreated = true;
-                }
+        boolean isCreated = false;
+        do {
+            int x = getRandomNumber(SIDE);
+            int y = getRandomNumber(SIDE);
+            if (gameField[y][x] == 0) {
+                gameField[y][x] = getRandomNumber(10) < 9 ? 2 : 4;
+                isCreated = true;
             }
-            while (!isCreated);
         }
+        while (!isCreated);
+    }
 
     private void drawScene() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                setCellColor(i, j, Color.RED);
+                setCellColoredNumber(i, j, gameField[j][i]);
             }
         }
+    }
+
+    private void setCellColoredNumber(int x, int y, int value) {
+        String str = value > 0 ? "" + value : "";
+        setCellValueEx(x, y, getColorByValue(value), str);
+    }
+
+    private Color getColorByValue(int value) {
+        Color color = Color.NONE;
+        switch (value) {
+            case 0: {
+                color = Color.WHITE;
+                break;
+            }
+            case 2: {
+                color = Color.BEIGE;
+                break;
+            }
+            case 4: {
+                color = Color.YELLOW;
+                break;
+            }
+            case 8: {
+                color = Color.RED;
+                break;
+            }
+            case 16: {
+                color = Color.ORANGE;
+                break;
+            }
+            case 32: {
+                color = Color.BLACK;
+                break;
+            }
+            case 64: {
+                color = Color.BLUE;
+                break;
+            }
+            case 128: {
+                color = Color.VIOLET;
+                break;
+            }
+            case 256: {
+                color = Color.TAN;
+                break;
+            }
+            case 512: {
+                color = Color.ROSYBROWN;
+                break;
+            }
+            case 1024: {
+                color = Color.WHEAT;
+                break;
+            }
+            case 2048: {
+                color = Color.ANTIQUEWHITE;
+                break;
+            }
+        }
+        return color;
     }
 
 }
