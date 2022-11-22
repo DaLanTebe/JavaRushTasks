75e8d6680ec9b770e8a470db325d2fd029a4cbcf
daniil <danilka.radaev.00@mail.ru>
2022-11-22 20:30:47 +0300

    add solution javaGames from game2048.3/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -5,10 +5,15 @@ import com.javarush.engine.cell.Game;
 public class Game2048 extends Game {
 
     private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame(){
     }
 
     public static void main(String[] args) {
