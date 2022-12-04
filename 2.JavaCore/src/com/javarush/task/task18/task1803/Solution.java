49ca76c85fd1bc36f1f506f43b8a9e3e667665ff
daniil <danilka.radaev.00@mail.ru>
2022-12-04 20:49:13 +0300

    add solution javaCore from task 1803

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -13,7 +13,6 @@ import java.util.stream.IntStream;
 public class Solution {
     public static void main(String[] args) throws Exception {
 
-        ArrayList<Integer> list = new ArrayList<>();
         int[] byteArray = new int[256];
         try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
         FileInputStream fileInputStream = new FileInputStream(console.readLine())){
@@ -21,7 +20,7 @@ public class Solution {
                 byteArray[fileInputStream.read()]++;
             }
         }
-        IntStream.range(0, byteArray.length).filter(i -> byteArray[i] == Arrays.stream(byteArray).max().getAsInt()).forEach(list::add);
-        list.forEach(i -> System.out.print(i + " "));
+        IntStream.range(0, byteArray.length).filter(i -> byteArray[i] == Arrays.stream(byteArray).max().getAsInt()).boxed().toList().forEach(integer -> System.out.print(integer + " "));
+
     }
 }
