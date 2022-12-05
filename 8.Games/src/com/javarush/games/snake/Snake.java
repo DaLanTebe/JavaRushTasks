0652ed0a5a1c3def2ef239bf0a85e6446d417efc
daniil <danilka.radaev.00@mail.ru>
2022-12-05 18:21:39 +0300

    add solution javaGames from SnakeGame.6/20

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -1,9 +1,14 @@
 package com.javarush.games.snake;
 
+import com.javarush.engine.cell.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
+
+    private static final String HEAD_SIGN ="\uD83D\uDC7E";
+    private static final String BODY_SIGN ="\u26AB";
     private List<GameObject> snakeParts = new ArrayList<>();
 
     public Snake(int x, int y) {
@@ -13,5 +18,12 @@ public class Snake {
         snakeParts.add(obj1);
         snakeParts.add(obj2);
         snakeParts.add(obj3);
+    }
+
+    public void draw(Game game){
+        for (int i = 0; i < snakeParts.size(); i++) {
+            String sign = i == 0 ? HEAD_SIGN : BODY_SIGN;
+                game.setCellValue(snakeParts.get(i).x,snakeParts.get(i).y, sign);
+        }
     }
 }
--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -6,21 +6,26 @@ public class SnakeGame extends Game {
 
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
+    private Snake snake;
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
     }
-    private void createGame(){
+
+    private void createGame() {
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
         drawScene();
 
     }
-    private void drawScene(){
+
+    private void drawScene() {
         for (int i = 0; i < WIDTH; i++) {
             for (int j = 0; j < HEIGHT; j++) {
-                setCellColor(i,j, Color.ANTIQUEWHITE);
+                setCellColor(i, j, Color.ANTIQUEWHITE);
             }
         }
+        snake.draw(this);
     }
 }
