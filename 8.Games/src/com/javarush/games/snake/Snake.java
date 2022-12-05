ac3283a12b8b687a744e0c81e26a3f2191e6e823
daniil <danilka.radaev.00@mail.ru>
2022-12-05 18:33:18 +0300

    add solution javaGames from SnakeGame.7/20

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -10,6 +10,12 @@ public class Snake {
     private static final String HEAD_SIGN ="\uD83D\uDC7E";
     private static final String BODY_SIGN ="\u26AB";
     private List<GameObject> snakeParts = new ArrayList<>();
+    public boolean isAlive = true;
+    private Direction direction = Direction.LEFT;
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
 
     public Snake(int x, int y) {
         GameObject obj1 = new GameObject(x, y);
@@ -23,7 +29,9 @@ public class Snake {
     public void draw(Game game){
         for (int i = 0; i < snakeParts.size(); i++) {
             String sign = i == 0 ? HEAD_SIGN : BODY_SIGN;
-                game.setCellValue(snakeParts.get(i).x,snakeParts.get(i).y, sign);
+            if (isAlive){
+                game.setCellValueEx(snakeParts.get(i).x,snakeParts.get(i).y,Color.NONE, sign,Color.DARKGREEN, 75);
+            }else game.setCellValueEx(snakeParts.get(i).x,snakeParts.get(i).y,Color.NONE, sign,Color.RED, 75);
         }
     }
 }
