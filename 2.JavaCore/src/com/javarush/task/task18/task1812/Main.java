ab49fd368fec0771a8d64322eae337c87199ee6b
daniil <danilka.radaev.00@mail.ru>
2022-12-14 19:54:53 +0300

    add solution javaCore from task 1812

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1812/Main.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task18.task1812;
+
+public class Main {
+    public static void main(String[] args) {
+        SupeAmigo supeAmigo = new SupeAmigo();
+        QuestionFileOutputStream stream = new QuestionFileOutputStream(supeAmigo);
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task18/task1812/SupeAmigo.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task18.task1812;
+
+import java.io.IOException;
+
+public class SupeAmigo implements AmigoOutputStream{
+    @Override
+    public void flush() throws IOException {
+
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
