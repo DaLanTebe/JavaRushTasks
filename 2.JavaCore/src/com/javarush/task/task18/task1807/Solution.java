1da33a843c231f9211ec9183d73be3684926d37c
daniil <danilka.radaev.00@mail.ru>
2022-12-18 19:17:01 +0300

    add solution javaCore from task 1807

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task18.task1807;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Подсчет запятых
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream inputStream = new FileInputStream(console.readLine())){
+            int count = 0;
+            while (inputStream.available() > 0){
+                if (inputStream.read() == 44){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
