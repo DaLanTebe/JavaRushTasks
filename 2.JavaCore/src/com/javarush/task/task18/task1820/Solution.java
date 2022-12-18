642b5ffcc849baa21ab5c1399678bea62a543859
daniil <danilka.radaev.00@mail.ru>
2022-12-18 19:06:30 +0300

    add solution javaCore from task 1820

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task18.task1820;
+
+import java.io.*;
+
+/* 
+Округление чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new FileReader(console.readLine()));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(console.readLine()))){
+
+            while (reader.ready()){
+                String[] strings = reader.readLine().split(" ");
+                StringBuilder builder = new StringBuilder();
+                for (String string : strings) {
+                    builder.append((int) Math.round(Double.parseDouble(string))).append(" ");
+                }
+                writer.write(builder + System.lineSeparator());
+            }
+        }
+    }
+}
