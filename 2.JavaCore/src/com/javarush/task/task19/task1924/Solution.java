f859922297dbac82577eadb45aa08a02c17d3585
daniil <danilka.radaev.00@mail.ru>
2023-01-04 17:29:29 +0300

    add solution javaCore from task 1924

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task19.task1924;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Замена чисел
+*/
+
+public class Solution {
+    public static Map<Integer, String> map = new HashMap<Integer, String>();
+
+//    static {
+//        map.put(1,"один");
+//        map.put(2,"два");
+//        map.put(,);
+//        map.put(,);
+//        map.put(,);
+//        map.put(,);
+//        map.put(,);
+//        map.put(,);
+//        map.put(,);
+//        map.put(,);
+//        map.put(,);
+//        map.put(,);
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
