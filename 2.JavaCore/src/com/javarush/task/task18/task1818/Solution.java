1a3869dead039f7799f8caad6a61a5548dc1d258
daniil <danilka.radaev.00@mail.ru>
2023-01-05 12:26:34 +0300

    add solution javaCore from task 1818

--- 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
@@ -7,7 +7,17 @@ import java.io.*;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        FileWriter writer1 = new FileWriter(console.readLine());
+        BufferedReader reader2 = new BufferedReader(new FileReader(console.readLine()));
+        BufferedReader reader3 = new BufferedReader(new FileReader(console.readLine()));){
+            while (reader2.ready()){
+                writer1.write(reader2.readLine());
+            }
+            while (reader3.ready()){
+                writer1.write(reader3.readLine());
+            }
+        }
     }
 }
