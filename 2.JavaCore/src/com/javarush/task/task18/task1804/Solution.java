306be8c8d598930747305f3fbf4e06ef429d35be
daniil <danilka.radaev.00@mail.ru>
2023-01-05 12:41:40 +0300

    add solution javaCore from task 1804

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -3,7 +3,8 @@ package com.javarush.task.task18.task1804;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.IntStream;
 
 /* 
 Самые редкие байты
@@ -11,5 +12,17 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        int[] byteArray = new int[256];
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream fileInputStream = new FileInputStream(console.readLine())) {
+            while (fileInputStream.available() > 0) {
+                byteArray[fileInputStream.read()]++;
+            }
+        }
+        int[] ints = IntStream.range(0, byteArray.length).filter(i -> byteArray[i] == Arrays.stream(byteArray).min().getAsInt()).toArray();
+        for (int i = 0; i < ints.length; i++) {
+            System.out.print(ints[i] + " ");
+        }
     }
 }
