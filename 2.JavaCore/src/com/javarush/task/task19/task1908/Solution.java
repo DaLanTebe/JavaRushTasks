59a55805f2cdacc91c22574dfb74524ff99eaa08
daniil <danilka.radaev.00@mail.ru>
2023-01-12 22:16:24 +0300

    add solution javaCore from task 1908

--- 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task19.task1908;
 
 import java.io.*;
+import java.text.ParseException;
 import java.util.ArrayList;
 
 /* 
@@ -8,6 +9,19 @@ import java.util.ArrayList;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new FileReader(console.readLine()));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(console.readLine()))){
+            while (reader.ready()){
+                String s = reader.readLine();
+                String[] split = s.split(" ");
+                for (String string : split) {
+                    if (string.matches("\\d+")){
+                        writer.write(string + " ");
+                    }
+                }
+            }
+        }
     }
 }
