883afb30ec0f5957422eb85b762f3a832ee7c7eb
daniil <danilka.radaev.00@mail.ru>
2023-01-12 19:32:10 +0300

    add solution javaCore from task 1921

--- 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -1,10 +1,9 @@
 package com.javarush.task.task19.task1921;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /* 
 Хуан Хуанович
@@ -13,15 +12,16 @@ import java.util.List;
 public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<Person>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))){
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
             while (reader.ready()){
-                String[] strings = reader.readLine().split(" ");
-                for (String string : strings) {
-
-                }
-                PEOPLE.add(new Person());
+                String string = reader.readLine();
+                String name = string.replaceAll("\\d", "").trim();
+                String date = string.replaceAll("\\D", " ").trim();
+                PEOPLE.add(new Person(name, dateFormat.parse(date)));
             }
         }
+        PEOPLE.forEach(person -> System.out.println(person.getName() + " " + person.getBirthDate()));
     }
 }
