b5a17118de4f3df747b11b6dc4ab5610ecf45403
daniil <danilka.radaev.00@mail.ru>
2023-01-18 18:58:33 +0300

    add solution javaCore from task 1924

--- 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
@@ -15,7 +15,32 @@ import java.util.Map;
 public class Solution {
     public static Map<Integer, String> map = new HashMap<Integer, String>();
 
-    public static void main(String[] args) {
+    static {
+        map.put(0, "ноль");
+        map.put(1, "один");
+        map.put(2,"два");
+        map.put(3,"три");
+        map.put(4,"четыре");
+        map.put(5,"пять");
+        map.put(6,"шесть");
+        map.put(7,"семь");
+        map.put(8,"восемь");
+        map.put(9,"девять");
+        map.put(10,"десять");
+        map.put(11,"одиннадцать");
+        map.put(12,"двенадцать");
+    }
 
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new FileReader(console.readLine()))){
+            while (reader.ready()){
+                String string = reader.readLine();
+                for (Map.Entry<Integer,String> entry: map.entrySet()) {
+                   string = string.replaceAll("\\b" + entry.getKey() + "\\b", entry.getValue());
+                }
+                System.out.println(string);
+            }
+        }
     }
 }
