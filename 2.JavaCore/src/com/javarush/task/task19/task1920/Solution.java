39a2034979cbb9028ec36605f20eaecdaa670939
daniil <danilka.radaev.00@mail.ru>
2023-01-18 18:23:34 +0300

    add solution javaCore from task 1920

--- 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
@@ -3,14 +3,35 @@ package com.javarush.task.task19.task1920;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 /* 
 Самый богатый
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        Map<String, Double> map = new TreeMap<>();
+        Set<String> set = new HashSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))){
+            while (reader.ready()){
+                String line = reader.readLine();
+                String[] strings = line.split(" ");
+                String name = strings[0];
+                double aDouble = Double.parseDouble(strings[1]);
+                map.merge(name, aDouble, Double::sum);
+            }
+            double max = Double.MIN_VALUE;
+            for (Map.Entry<String, Double> entry : map.entrySet()) {
+                if (entry.getValue() > max){
+                    max = entry.getValue();
+                    set.clear();
+                    set.add(entry.getKey());
+                }else if (entry.getValue() == max){
+                    set.add(entry.getKey());
+                }
+            }
+            set.forEach(System.out::println);
+        }
     }
 }
