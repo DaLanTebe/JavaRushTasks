1c73baea31b666c54ee13da1589581b296ebfc2b
daniil <danilka.radaev.00@mail.ru>
2023-01-18 20:46:13 +0300

    add solution javaCore from task 1925

--- 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task19.task1925;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 /* 
@@ -11,7 +8,20 @@ import java.util.ArrayList;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException{
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+             FileWriter writer = new FileWriter(args[1])){
+            while(reader.ready()){
+                String[] strings = reader.readLine().split(" ");
+                for (String string : strings) {
+                    if(string.length() > 6){
+                        builder.append(string + ",");
+                    }
+                }
+            }
+            builder.deleteCharAt(builder.length() - 1);
+            writer.write(builder.toString());
+        }
     }
 }
