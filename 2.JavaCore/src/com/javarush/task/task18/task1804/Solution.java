8d135f0dc8dc07bbfd0e032db5df566f38c0ce87
daniil <danilka.radaev.00@mail.ru>
2023-01-25 18:56:23 +0300

    add solution javaCore from task 1804

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -2,8 +2,10 @@ package com.javarush.task.task18.task1804;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /* 
@@ -12,7 +14,6 @@ import java.util.stream.IntStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-
         int[] byteArray = new int[256];
         try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
              FileInputStream fileInputStream = new FileInputStream(console.readLine())) {
@@ -20,9 +21,39 @@ public class Solution {
                 byteArray[fileInputStream.read()]++;
             }
         }
-        int[] ints = IntStream.range(0, byteArray.length).filter(i -> byteArray[i] == Arrays.stream(byteArray).min().getAsInt()).toArray();
-        for (int i = 0; i < ints.length; i++) {
-            System.out.print(ints[i] + " ");
+//        final int minimum = Integer.MAX_VALUE;
+//        IntStream.range(0, byteArray.length)
+//                .filter(i -> i > 0)
+//                .filter(i -> byteArray[i] == Arrays.stream(byteArray).min().getAsInt())
+//                .boxed()
+//                .collect(Collectors.toList())
+//                .forEach(index -> System.out.print(index + " "));
+//
+//
+        int min = Integer.MAX_VALUE;
+        for (int count : byteArray) {
+            if (count > 0 && count < min) {
+                min = count;
+            }
         }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < byteArray.length; i++) {
+            if (byteArray[i] == min) {
+                list.add(i);
+            }
+        }
+        list.forEach(index -> System.out.print(index + " "));
+    }
+
+    private static void method() throws IOException {
+        Map<Integer, Integer> map = new HashMap<>();
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream fileInputStream = new FileInputStream(console.readLine())) {
+            while (fileInputStream.available() > 0) {
+                map.merge(fileInputStream.read(), 1, Integer::sum);
+            }
+        }
+
+        map.forEach((k,v) -> System.out.println(Objects.equals(v, map.values().stream().min(Integer::compareTo).get()) ? k + " " : ""));
     }
 }
