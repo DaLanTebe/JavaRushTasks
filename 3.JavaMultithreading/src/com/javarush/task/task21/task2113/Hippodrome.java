1c0b147574a9a6bfda6e05ee61f7c2047c052528
daniil <danilka.radaev.00@mail.ru>
2023-02-06 20:28:53 +0300

    add solution JavaMultithreading from 2113 8/16

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -24,4 +24,18 @@ public class Hippodrome {
     public List<Horse> getHorses() {
         return horses;
     }
+
+    public void run() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+    }
+    public void move(){
+
+    }
+    public void print(){
+        
+    }
 }
