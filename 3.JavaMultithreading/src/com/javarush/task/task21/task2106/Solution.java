127b7ef25a0900c92eca86409b83e4815722a4c1
daniil <danilka.radaev.00@mail.ru>
2023-02-08 20:02:20 +0300

    add solution JavaMultithreading from 2106

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2106/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2106/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task21.task2106;
 
 import java.util.Date;
+import java.util.Objects;
 
 /* 
 Ошибка в equals/hashCode
@@ -24,17 +25,16 @@ public class Solution {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o instanceof Solution) return false;
+        if (o == null) return false;
+        if (!(o instanceof Solution)) return false;
 
         Solution solution1 = (Solution) o;
 
-        if (Double.compare(solution1.aDouble, aDouble) != 0) return false;
-        if (anInt != solution1.anInt) return false;
-        if (date != null ? !date.equals(solution1.date) : solution1.date == null) return false;
-        if (solution != null ? !solution.equals(solution1.solution) : solution1.solution == null) return false;
-        if (string != null ? !string.equals(solution1.string) : solution1.string == null) return false;
-
-        return true;
+        return Objects.equals(anInt, solution1.anInt) &&
+                Objects.equals(string, solution1.string) &&
+                Objects.equals(aDouble, solution1.aDouble) &&
+                Objects.equals(date, solution1.date) &&
+                Objects.equals(solution, solution1.solution);
     }
 
     @Override
