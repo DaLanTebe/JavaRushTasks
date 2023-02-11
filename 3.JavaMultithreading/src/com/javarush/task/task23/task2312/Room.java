bee10f68c0121432fb62ce3c8355267a8635c002
daniil <danilka.radaev.00@mail.ru>
2023-02-11 21:44:06 +0300

    add solution JavaMultithreading from 2312

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -5,6 +5,7 @@ public class Room {
     private int height;
     private Snake snake;
     private Mouse mouse;
+    public static Room game;
 
     public Room(int wight, int height, Snake snake) {
         this.width = wight;
@@ -13,6 +14,10 @@ public class Room {
     }
 
     public static void main(String[] args) {
+        Snake newSnake = new Snake(10, 10);
+        newSnake.setDirection(SnakeDirection.DOWN);
+        game = new Room(20, 20, newSnake);
+
     }
 
     public int getWidth() {
@@ -46,4 +51,7 @@ public class Room {
     public void setMouse(Mouse mouse) {
         this.mouse = mouse;
     }
+
+    public void run(){}
+    public void print(){}
 }
