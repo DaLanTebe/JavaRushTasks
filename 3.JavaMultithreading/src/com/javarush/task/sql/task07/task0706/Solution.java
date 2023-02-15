236b895482f44be0bec075ea1f074206ea8b2208
daniil <danilka.radaev.00@mail.ru>
2023-02-15 19:07:38 +0300

    add solution SQLAndHibernate from 0706

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0706/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.sql.task07.task0706;
+
+import java.sql.*;
+
+/* 
+task0706
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        try (Connection connection = DriverManager.getConnection("" +
+                "jdbc:mysql://localhost:3306/test", "root", "root");
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM employee LIMIT 1")){
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                System.out.println(metaData.getColumnName(i) + "(" + metaData.getColumnTypeName(i) + ")");
+            }
+        }
+
+    }
+}
