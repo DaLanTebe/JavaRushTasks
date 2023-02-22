dbee90879706592cda98f2095ae5667b72b6cb65
daniil <danilka.radaev.00@mail.ru>
2023-02-22 20:04:18 +0300

    add solution JavaMultithreading from 2413.13

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,8 +1,52 @@
 package com.javarush.task.task24.task2413;
 
-public class Ball extends BaseObject{
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+public class Ball extends BaseObject {
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+
+        this.direction = direction;
+        this.speed = speed;
+
+        this.isFrozen = true;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
     }
 
     @Override
