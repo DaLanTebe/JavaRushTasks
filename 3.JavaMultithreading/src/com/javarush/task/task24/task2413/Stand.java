44e69a3604b0c660ad15e4db54f4ee467e3645c1
daniil <danilka.radaev.00@mail.ru>
2023-02-22 20:34:13 +0300

    add solution JavaMultithreading from 2413.17

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -33,6 +33,13 @@ public class Stand extends BaseObject{
 
     @Override
     public void move() {
+        x = x + speed * direction;
+    }
 
+    public void moveLeft(){
+        direction = -1;
+    }
+    public void moveRight(){
+        direction = 1;
     }
 }
