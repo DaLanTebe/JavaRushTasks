c3946c7261e47051e9dda6bd4f25913b2361a1ab
daniil <danilka.radaev.00@mail.ru>
2023-02-28 19:56:38 +0300

    add solution JavaMultithreading from 2401

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    public void Method1(){
+
+    }
+    public void Method2(){
+
+    }
+}
