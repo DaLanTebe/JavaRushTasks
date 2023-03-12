eadf8bbcb6f129d9e07f21c37f27509f825cc41e
daniil <danilka.radaev.00@mail.ru>
2023-03-12 19:55:01 +0300

    add solution JavaMultithreading from 3008 Chat 8/22

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
@@ -15,7 +16,7 @@ public class Server {
             try {
                 connection.send(message);
             } catch (IOException e) {
-                ConsoleHelper.writeMessage("не смогли отправить сообщение :" + connection.getRemoteSocketAddress());
+                ConsoleHelper.writeMessage("не смогли отправить сообщение : " + connection.getRemoteSocketAddress());
             }
         }
     }
@@ -33,14 +34,31 @@ public class Server {
             ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера");
         }
     }
-    private static class Handler extends Thread{
+    private static class Handler extends Thread {
         private Socket socket;
 
-        public Handler(Socket socket){
+        public Handler(Socket socket) {
             this.socket = socket;
         }
+
         @Override
         public void run() {
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST, "Пожалуйста введите имя пользователя"));
+                Message message = connection.receive();
+                String name = message.getData();
+                if (message.getType().equals(MessageType.USER_NAME) && !Objects.isNull(name) &&
+                        !name.isEmpty() && !connectionMap.containsKey(name)) {
+                    connectionMap.put(name, connection);
+                    connection.send(new Message(MessageType.NAME_ACCEPTED, "Добро пожаловть в чат, " + name));
+                    return name;
+                } else {
+                    ConsoleHelper.writeMessage("Ошибка ввода имени пользователя");
+                }
+            }
         }
     }
 }
