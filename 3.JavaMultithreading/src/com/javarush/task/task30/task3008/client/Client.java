5b4e5db34aad39e7ee13fe29620cf98c01572620
daniil <danilka.radaev.00@mail.ru>
2023-03-15 18:46:54 +0300

    add solution JavaMultithreading from 3008 12/22

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+
+public class Client {
+    private volatile boolean clientConnected = false;
+    protected Connection connection;
+    public class SocketThread extends Thread{
+    }
+}
