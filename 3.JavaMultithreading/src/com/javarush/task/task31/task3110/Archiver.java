493bb7c5c39fcfdcd20e23f018c92df063e0ceff
daniil <danilka.radaev.00@mail.ru>
2023-03-30 18:46:55 +0300

    add solution JavaMultithreading from 3110 1/18

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        ConsoleHelper.writeMessage("Введите абсолютный путь с клавиатуры");
+        String path = ConsoleHelper.readString();
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(path));
+        ConsoleHelper.writeMessage("Введите путь к файлу который будет архивирован");
+        String pathToArchive = ConsoleHelper.readString();
+        zipFileManager.createZip(Paths.get(pathToArchive));
+    }
+}
