d202d3146237a13d613a77d75caa3512643f3c16
daniil <danilka.radaev.00@mail.ru>
2023-04-02 18:35:38 +0300

    add solution JavaMultithreading from 3110 10/18

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -13,23 +13,30 @@ public class FileManager {
 
     public FileManager(Path rootPath) throws IOException {
         this.rootPath = rootPath;
-        fileList = new ArrayList<>();
+        this.fileList = new ArrayList<>();
         collectFileList(rootPath);
     }
 
+    public List<Path> getFileList() {
+        return fileList;
+    }
+
     private void collectFileList(Path path) throws IOException {
-        if (Files.isRegularFile(path)){
-            fileList.add(rootPath.relativize(path));
-        } else if (Files.isDirectory(path)) {
-            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)){
+        // Добавляем только файлы
+        if (Files.isRegularFile(path)) {
+            Path relativePath = rootPath.relativize(path);
+            fileList.add(relativePath);
+        }
+
+        // Добавляем содержимое директории
+        if (Files.isDirectory(path)) {
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
                 for (Path file : directoryStream) {
                     collectFileList(file);
                 }
             }
         }
-    }
-
-    public List<Path> getFileList() {
-        return fileList;
     }
 }
