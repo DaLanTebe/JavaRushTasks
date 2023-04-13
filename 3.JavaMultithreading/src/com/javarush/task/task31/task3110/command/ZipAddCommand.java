044cb6c8693b6e3d52959da16d957ccef60454b4
daniil <danilka.radaev.00@mail.ru>
2023-04-13 19:44:44 +0300

    add solution JavaMultithreading from 3110 18/18

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
@@ -10,13 +10,20 @@ import java.nio.file.Paths;
 public class ZipAddCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-        ConsoleHelper.writeMessage("Файл добавлен в архив");
         try {
+            ConsoleHelper.writeMessage("Добавление нового файла в архив.");
+
             ZipFileManager zipFileManager = getZipFileManager();
-            ConsoleHelper.writeMessage("Введите полный путь файла для добавления:");
-            zipFileManager.addFile(Paths.get(ConsoleHelper.readString()));
+
+            ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+
+            zipFileManager.addFile(sourcePath);
+
+            ConsoleHelper.writeMessage("Добавление в архив завершено.");
+
         } catch (PathIsNotFoundException e) {
-            ConsoleHelper.writeMessage("Файл не был найден");
+            ConsoleHelper.writeMessage("Файл не был найден.");
         }
     }
 }
