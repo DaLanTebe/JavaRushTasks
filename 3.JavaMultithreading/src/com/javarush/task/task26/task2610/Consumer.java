61be31ceeb520620a64be8f3e40fd0a15425a9d5
daniil <danilka.radaev.00@mail.ru>
2023-04-25 15:25:31 +0300

    add solution JavaMultithreading from 2610

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2610/Consumer.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task26.task2610;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Consumer implements Runnable{
+    private BlockingQueue queue;
+
+    public Consumer(BlockingQueue blockingQueue) {
+        this.queue = blockingQueue;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            try {
+                System.out.println(queue.take().toString());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
