d15a2d1248c1e812c0ea0f883aa036ae023d5eb2
daniil <danilka.radaev.00@mail.ru>
2023-05-15 18:43:45 +0300

    add solution JavaMultithreading from 2805

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
@@ -2,21 +2,36 @@ package com.javarush.task.task28.task2805;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MyThread extends Thread{
-    private static AtomicInteger priority = new AtomicInteger(1);
-    private static AtomicInteger counter = new AtomicInteger(1);
+public class MyThread extends Thread {
+//    private static AtomicInteger priority = new AtomicInteger(1);
+//    private static AtomicInteger counter = new AtomicInteger(1);
+//
+//    {
+//        if (counter.get() > MAX_PRIORITY) {
+//            priority.set(MIN_PRIORITY);
+//        }
+//        counter.set(priority.get());
+//        if (getThreadGroup().getMaxPriority() < priority.get()) {
+//            setPriority(getThreadGroup().getMaxPriority());
+//            priority.incrementAndGet();
+//        }else {
+//            setPriority(priority.getAndIncrement());
+//        }
+//        counter.incrementAndGet();
+//    }
 
+    private static AtomicInteger integer = new AtomicInteger(1);
     {
-        if (counter.get() == 10){
-            counter.set(0);
-            priority.set(1);
+        int priority = integer.getAndIncrement();
+        if (priority > MAX_PRIORITY){
+            priority = MIN_PRIORITY;
         }
-            if (this.getThreadGroup().getMaxPriority() < priority.get()) {
-                this.setPriority(this.getThreadGroup().getMaxPriority());
-        }else this.setPriority(priority.getAndIncrement());
-        counter.incrementAndGet();
+        integer.set(priority);
+        setPriority(integer.getAndIncrement());
+        if (getThreadGroup().getMaxPriority() < priority){
+            setPriority(getThreadGroup().getMaxPriority());
+        }
     }
-
     public MyThread() {
 
     }
