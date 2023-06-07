4446b28e88fcab7bd5a1dc87d597e8bd88765481
daniil <danilka.radaev.00@mail.ru>
2023-06-07 23:04:47 +0300

    add solution JavaRushGames Minesweeper 5/16

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -5,6 +5,7 @@ import com.javarush.engine.cell.*;
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+    private int countMinesOnField;
 
     @Override
     public void initialize() {
@@ -15,7 +16,10 @@ public class MinesweeperGame extends Game {
     private void createGame() {
         for (int x = 0; x < SIDE; x++) {
             for (int y = 0; y < SIDE; y++) {
-                gameField[y][x] = new GameObject(x, y);
+                if (getRandomNumber(10) == 0){
+                    gameField[y][x] = new GameObject(x, y, true);
+                    countMinesOnField++;
+                }else gameField[y][x] = new GameObject(x, y, false);
                 setCellColor(x, y, Color.ALICEBLUE);
             }
         }
