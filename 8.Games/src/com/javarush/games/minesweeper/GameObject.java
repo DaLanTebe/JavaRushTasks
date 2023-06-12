fde00efe6640e3a6c1f6d1a6b9b7cf853897dc9e
daniil <danilka.radaev.00@mail.ru>
2023-06-12 19:56:02 +0300

    add solution JavaRushGames Minesweeper 8/16

--- 8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -6,6 +6,7 @@ public class GameObject {
     public boolean isMine;
     public int countMineNeighbors;
     public boolean isOpen = false;
+    public boolean isFlag = false;
 
     public GameObject(int x, int y, boolean isMine) {
         this.x = x;
