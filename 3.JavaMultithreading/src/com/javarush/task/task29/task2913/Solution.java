89f1e07e86f5e748b9acc74a471e308adc4e290d
daniil <danilka.radaev.00@mail.ru>
2023-06-14 18:36:05 +0300

    add solution JavaMultithreading from 2913

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
@@ -2,6 +2,8 @@ package com.javarush.task.task29.task2913;
 
 
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /* 
 Замена рекурсии
@@ -11,12 +13,10 @@ public class Solution {
     private static int numberA;
     private static int numberB;
 
-    public static String getAllNumbersBetween(int a, int b) {
+    public static String getAllNumbersBetween1 (int a, int b) {
         StringBuilder builder = new StringBuilder();
         if (b > a){
-            for (int i = a; i <= b; i++) {
-                builder.append(i).append(" ");
-            }
+            IntStream.rangeClosed(a, b).forEach(value -> builder.append(value).append(" "));
             return builder.toString().trim();
         }  if (b < a) {
             for (int i = a; i >= b; i--) {
@@ -25,6 +25,12 @@ public class Solution {
             return builder.toString().trim();
         }
         return Integer.toString(a);
+    }
+
+    public static String getAllNumbersBetween(int a, int b){
+       return b > a ?
+               IntStream.rangeClosed(a, b).mapToObj(i -> i + " ").collect(Collectors.joining()).trim() :
+               IntStream.iterate(a, i -> i >= b, i -> i - 1).mapToObj(i -> i + " ").collect(Collectors.joining()).trim();
     }
 
     public static void main(String[] args) {
