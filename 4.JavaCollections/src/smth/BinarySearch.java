18fafe9e15ca2789331b59c95ef6b7bb077dc069
danaradaev <danilka.radaev.00@mail.ru>
2024-08-23 11:57:23 +0300

    BinarySearch

---
+++ 4.JavaCollections/src/smth/BinarySearch.java
@@ -0,0 +1,34 @@
+package smth;
+
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        long currentTimeMillis = System.currentTimeMillis();
+        int[] ints = new int[1000000];
+        for (int i = 0; i < ints.length - 1; i++) {
+            ints[i] = (int) (Math.random() * 10000);
+        }
+        ints[100001] = 8;
+        Arrays.sort(ints);
+        System.out.println(STR."\{binarySearch(ints, 8, 0, ints.length - 1)} \{System.currentTimeMillis()}");
+    }
+    private static int binarySearch (int[] sortedArray, int valueToFind, int low, int high){
+        int index = -1;
+
+        while (low <= high){
+            int mid = low + (high - low)  / 2;
+            if (sortedArray[mid] < valueToFind){
+                low = mid + 1;
+            } else if (sortedArray[mid] > valueToFind) {
+                high = mid - 1;
+            } else if (sortedArray[mid] == valueToFind) {
+                index = mid;
+                break;
+            }
+        }
+
+        return index;
+    }
+}
