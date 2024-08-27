0e50c2669821a63986de1d52399c09034889f3b7
danaradaev <danilka.radaev.00@mail.ru>
2024-08-27 18:06:26 +0300

    task3205 Proxy-Object

---
+++ 4.JavaCollections/src/task3205ProxyObject/CustomInvocationHandler.java
@@ -0,0 +1,21 @@
+package task3205ProxyObject;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class CustomInvocationHandler implements InvocationHandler {
+
+    SomeInterfaceWithMethods inter;
+
+    public CustomInvocationHandler(SomeInterfaceWithMethods inter){
+        this.inter = inter;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println(method.getName() + " in");
+        Object invoke = method.invoke(inter, args);
+        System.out.println(method.getName() + " out");
+        return invoke;
+    }
+}
---
+++ 4.JavaCollections/src/task3205ProxyObject/SomeInterfaceWithMethods.java
@@ -0,0 +1,10 @@
+package task3205ProxyObject;
+
+public interface SomeInterfaceWithMethods {
+    void voidMethodWithoutArgs();
+
+    String stringMethodWithoutArgs();
+
+    void voidMethodWithIntArg(int i);
+}
+
