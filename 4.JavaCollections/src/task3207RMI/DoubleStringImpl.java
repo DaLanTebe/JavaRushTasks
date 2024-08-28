cd24cb59980a28aca84350f2ed019fbfc3b3a222
danaradaev <danilka.radaev.00@mail.ru>
2024-08-28 17:09:00 +0300

    task3207 RMI_1

---
+++ 4.JavaCollections/src/task3207RMI/DoubleStringImpl.java
@@ -0,0 +1,9 @@
+package task3207RMI;
+
+import java.rmi.RemoteException;
+
+public class DoubleStringImpl implements DoubleString {
+    public String doubleString(String str) throws RemoteException {
+        return str + str;
+    }
+}
