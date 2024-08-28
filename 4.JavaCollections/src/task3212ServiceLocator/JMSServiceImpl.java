30a8e9350e26bb8a86057bb673ec59acb5d991bc
danaradaev <danilka.radaev.00@mail.ru>
2024-08-28 17:53:45 +0300

    task3212 ServiceLocator

---
+++ 4.JavaCollections/src/task3212ServiceLocator/JMSServiceImpl.java
@@ -0,0 +1,16 @@
+package task3212ServiceLocator;
+
+
+public class JMSServiceImpl implements Service {
+
+    @Override
+    public void execute() {
+        System.out.println("Executing the JMSService");
+    }
+
+    @Override
+    public String getName() {
+        return "JMSService";
+    }
+
+}
---
+++ 4.JavaCollections/src/task3212ServiceLocator/Service.java
@@ -0,0 +1,7 @@
+package task3212ServiceLocator;
+
+public interface Service {
+    String getName();
+
+    void execute();
+}
