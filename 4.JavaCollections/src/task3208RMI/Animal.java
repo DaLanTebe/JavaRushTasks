45262166a62acf468e62de15f0de4ea8fbc6bc77
danaradaev <danilka.radaev.00@mail.ru>
2024-08-28 17:30:54 +0300

    task3208 RMI_2

---
+++ 4.JavaCollections/src/task3208RMI/Animal.java
@@ -0,0 +1,14 @@
+package task3208RMI;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Animal extends Remote {
+    void speak() throws RemoteException;
+
+    void printName() throws RemoteException;
+}
+
+
+
+
