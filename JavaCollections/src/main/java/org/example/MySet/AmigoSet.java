8c69caeb68143eda34f33a28092ba29df04508df
danaradaev <danilka.radaev.00@mail.ru>
2024-09-07 15:13:44 +0300

    MySet 1.0

---
+++ JavaCollections/src/main/java/org/example/MySet/AmigoSet.java
@@ -0,0 +1,34 @@
+package org.example.MySet;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        this.map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        this.map = new HashMap<>(Math.max((int) Math.ceil(collection.size() / .75f), 16));
+        addAll(collection);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
