8d54480be75d294bc13d78b06c71d9d2649be2ee
danaradaev <danilka.radaev.00@mail.ru>
2024-09-12 18:22:17 +0300

    task 3507

--- JavaCollections/src/main/java/org/example/task3507/Solution.java
+++ JavaCollections/src/main/java/org/example/task3507/Solution.java
@@ -2,7 +2,9 @@ package org.example.task3507;
 
 import java.io.*;
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /*
@@ -10,19 +12,34 @@ ClassLoader - что это такое?
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Set<? extends Animal> allAnimals = getAllAnimals(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/data");
         System.out.println(allAnimals);
     }
 
-    public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
+    public static Set<? extends Animal> getAllAnimals(String pathToAnimals) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        HashSet<Animal> set = new HashSet<>();
+
         File path = new File(pathToAnimals);
-        File[] files = path.listFiles();
-        for (File file : files) {
-            System.out.println(file.getName());
+        List<File> list = Arrays.stream(path.listFiles()).filter(a -> a.getAbsolutePath().toString().matches("%.class")).toList();
+
+        MyClassLoader myClassLoader = new MyClassLoader();
+        for (File file : list) {
+            Class<? extends Animal> loaderClass = (Class<? extends Animal>) myClassLoader.findClass(file.getName());
+            for (Constructor constructor : loaderClass.getConstructors()){
+                if (constructor.getModifiers() == 1 && constructor.getParameterCount() == 0 && loaderClass.isAssignableFrom(Animal.class)){
+                    set.add(loaderClass.newInstance());
+                }
+            }
         }
-        HashSet<Animal> ts = new HashSet<>();
-        return ts;
+
+        return set;
+    }
+    private static class MyClassLoader extends ClassLoader{
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            return super.findClass(name);
+        }
     }
 }
 
